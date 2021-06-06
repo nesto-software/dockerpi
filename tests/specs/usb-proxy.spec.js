507b75279474a3cfb01efc505be29bf0d3e6934e
Martin Löper <martin.loeper@gmx.de>
2021-06-06 02:14:55 +0200

    feat: add more tolerance

--- tests/specs/usb-proxy.spec.js
+++ tests/specs/usb-proxy.spec.js
@@ -4,7 +4,8 @@ let ssh2Handle;
 
 beforeAll(async () => {
     await waitForShh();
-    console.log("Connection to virtual pi established.");
+    console.log("Connection to virtual pi established. Wait some more seconds to be sure...");
+    await new Promise((resolve) => setTimeout(resolve, 10000));
     ssh2Handle = await connect();
 }, 600000);
 
@@ -12,7 +13,7 @@ describe('USBProxy', () => {
     test('Installation', async () => {
         const cmd = 'sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/nesto-software/USBProxy/master/scripts/install-from-release.sh)"';
         await exec(ssh2Handle, cmd);
-    }, 120000);
+    }, 200000);
 
     test('Binary Existance', async () => {
         const cmd = 'which usb-mitm';
